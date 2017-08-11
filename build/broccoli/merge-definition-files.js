6deb3d090c327a9dfcb32994e2ed16fd267a634e
Tom Dale <tom@tomdale.net>
2017-08-11 16:34:13 -0400

    WIP

---
+++ build/broccoli/merge-definition-files.js
@@ -0,0 +1,14 @@
+const merge = require('broccoli-merge-trees');
+const funnel = require('broccoli-funnel');
+
+/**
+ * The TypeScript compiler doesn't re-emit input `.d.ts` files, so we manually
+ * merge type definitions directly from source into the built output.
+ */
+module.exports = function(tsTree, jsTree) {
+  let definitionsTree = funnel(tsTree, {
+    include: ['packages/@glimmer/**/*.d.ts']
+  });
+
+  return merge([jsTree, definitionsTree]);
+}
